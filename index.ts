e2709459c7ebea20772eba13b531f815e7e69f10
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2019-12-15 13:26:18 +0000

    create hash key to encrypt and decrypt

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as crypto from 'crypto'
+import { HexBase64Latin1Encoding } from 'crypto'
 
 export interface Config {
     secret: string
@@ -6,28 +7,30 @@ export interface Config {
         hash: string,
         cipher: string
     },
-    encoding: string
+    encoding: HexBase64Latin1Encoding
 }
 
 // references: 
 //          https://github.com/nodejs/node/issues/16746
 //          https://medium.com/@anned20/encrypting-files-with-nodejs-a54a0736a50a
 export class Encrypt {
-    constructor(private config: Config, private key: string) {
+    constructor(private config: Config) {
     }
 
     encrypt(buffer: Buffer): Buffer {
         const initializationVector = crypto.randomBytes(16)
-        const cipher = crypto.createCipheriv(this.config.algorithm.cipher, this.key, initializationVector)
+        const key = crypto.createHash(this.config.algorithm.hash).update(this.config.secret).digest(this.config.encoding).substr(0, 32)
+        const cipher = crypto.createCipheriv(this.config.algorithm.cipher, key, initializationVector)
         
         return Buffer.concat([initializationVector, cipher.update(buffer), cipher.final()])
     }
     
     decrypt(encrypted: Buffer): Buffer {
         const initializationVector = encrypted.slice(0, 16)
+        const key = crypto.createHash(this.config.algorithm.hash).update(this.config.secret).digest(this.config.encoding).substr(0, 32)
     
         encrypted = encrypted.slice(16)
-        const decipher = crypto.createDecipheriv('aes-256-ctr', this.key, initializationVector)
+        const decipher = crypto.createDecipheriv(this.config.algorithm.cipher, key, initializationVector)
         return Buffer.concat([decipher.update(encrypted), decipher.final()])
     }
 }
